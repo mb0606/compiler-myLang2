a9b0aed09f9547bc7d1949e194300e31bc1aad76
mb0606 <marcoberardini@gmail.com>
2020-11-01 16:27:45 -0800

    add function evaluate

--- src/evaluate.js
+++ src/evaluate.js
@@ -1,6 +1,21 @@
 const { environment } = require("./std-lib")
 const last = (collection) => collection[collection.length - 1]
 
-const evaluate = () => {}
+const apply = (node) => {
+  const func = environment[node.name]
+  const args = node.arguments.map(evaluate)
+  if (typeof func !== "function") {
+    throw new TypeError(`${node.name} is not a function`)
+  }
+  return func(...args)
+}
+const evaluate = (node) => {
+  if (node.type === "CallExpression") {
+    return apply(node)
+  }
+  if (node.value) {
+    return node.value
+  }
+}
 
 module.exports = { evaluate }
